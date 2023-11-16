a96f4c3624bc1cd2ca30659037e948fbe1f45dd1
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-16 22:58:12 +0100

    feat: random number

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const board = document.querySelector('.board');
+const playButton = document.querySelector('.play-btn'); 
+let bombNumber = 16;
 
 // funzione che crea elemento
 function myCreateElement(tag, className, content){
@@ -29,9 +31,8 @@ function setCellNumber(level){
     }
     return cellNumber;
 }
-
+// funzione che crea gli elementi nella board
 function creatBoard (mainElement, cellNumber){
-    
     
     const fragment = document.createDocumentFragment();
     const cells = Math.sqrt(cellNumber);
@@ -48,6 +49,27 @@ function creatBoard (mainElement, cellNumber){
     }
     mainElement.append(fragment);
 }
+// Funzione di reset
+function resetFn(){
+    board.innerHTML = "";
+}
+
+// Funzione genera numeri random
+function randomNumber(cellNumber) {
+    const bombArray = [];
+
+    while (bombArray.length < bombNumber){
+        const random = Math.floor(Math.random() * cellNumber + 1);
+
+        if(!bombArray.includes(random)){
+            bombArray.push(random);
+        }
+
+    }
+    return bombArray;
+
+}
+// Funzione principale
 function campoMinato(){
     resetFn()
     
@@ -57,14 +79,11 @@ function campoMinato(){
     console.log(levelStatus);
     console.log(cellNumber);
 
-    creatBoard(board, cellNumber);
-}
-// Funzione di reset
-function resetFn(){
-    board.innerHTML = "";
-}
+    let bombArray = randomNumber(cellNumber);
+    console.log(bombArray);
 
-const playButton = document.querySelector('.play-btn'); 
+    creatBoard(board, cellNumber, bombArray);
+}
 
 // Click al bottone compare la board
 playButton.addEventListener('click', campoMinato);
